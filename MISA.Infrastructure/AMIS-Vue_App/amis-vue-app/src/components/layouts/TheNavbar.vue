735107406304646d41a8c44a28f548b2c3916a65
hmducanh <qliphotducanh@gmail.com>
2021-05-09 23:30:56 +0700

    Add amis-vue-app

---
+++ MISA.Infrastructure/AMIS-Vue_App/amis-vue-app/src/components/layouts/TheNavbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="navbar">
+    <div class="logo-box">
+      <div class="toggle-logo-itemon"></div>
+      <div class="amis-logo"></div>
+    </div>
+    <!-- list item tren thanh navbar ben phai -->
+    <div class="navbar-content-list">
+      <div class="nav-item">
+        <div class="logo-item nav-item-1"></div>
+        <div class="nav-item-text">Tổng quan</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-2"></div>
+        <div class="nav-item-text">Tiền mặt</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-3"></div>
+        <div class="nav-item-text">Tiền gửi</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-4"></div>
+        <div class="nav-item-text">Mua hàng</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-5"></div>
+        <div class="nav-item-text">Bán hàng</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-6"></div>
+        <div class="nav-item-text">Quản lý hóa đơn</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-7"></div>
+        <div class="nav-item-text">Kho</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-8"></div>
+        <div class="nav-item-text">Công cụ dụng cụ</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-9"></div>
+        <div class="nav-item-text">Tài sản cố định</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-10"></div>
+        <div class="nav-item-text">Thuế</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-11"></div>
+        <div class="nav-item-text">Giá thành</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-12"></div>
+        <div class="nav-item-text">Tổng hợp</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-13"></div>
+        <div class="nav-item-text">Ngân sách</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-14"></div>
+        <div class="nav-item-text">Báo cáo</div>
+      </div>
+      <div class="nav-item">
+        <div class="logo-item nav-item-15"></div>
+        <div class="nav-item-text">Phân tích tài chính</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TheNavbar",
+};
+</script>
+
+<style></style>
---
+++ MISA.Infrastructure/AMIS-Vue_App/amis-vue-app/src/components/views/employee/EmployeeList.vue
@@ -0,0 +1,166 @@
+<template>
+    <div>
+        <div class = "content-body">
+      <div class = "content-title">
+          Nhân viên
+      </div>
+      <!-- button add employee -->
+      <div class = "buttonAdd" @click="btnAddOnClick()">
+        <button class = "btnAdd btn-default">Thêm một nhân viên</button>
+      </div>
+    </div>
+    <!-- input search va button refresh -->
+    <div class = "content-table">
+      <div class = "bar">
+            <input type="text" class = "search-table">
+            <div class = "refresh-button"></div>
+        </div>
+        <!-- Tao bang data -->
+        <div class = "grid">
+            <table id = "tblListEmployee" class = "table">
+                <thead>
+                    <tr>
+                        <th>
+                        </th>
+                        <th>MÃ NHÂN VIÊN</th>
+                        <th>TÊN NHÂN VIÊN</th>
+                        <th>GIỚI TÍNH</th>
+                        <th>NGÀY SINH</th>
+                        <th>SỐ CMND</th>
+                        <th>CHỨC DANH</th>
+                        <th>TÊN ĐƠN VỊ</th>
+                        <th>SỐ TÀI KHOẢN</th>
+                        <th>TÊN NGÂN HÀNG</th>
+                        <th>CHI NHÁNH TK NGÂN HÀNG</th>
+                        <th>CHỨC NĂNG</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr @dblclick="trOnDbClick()">
+                        <td></td>
+                        <td>NV0000</td>
+                        <td>Nguyễn Phú Trọng</td>
+                        <td>Nam</td>
+                        <td>01/01/1200</td>
+                        <td>001200003322</td>
+                        <td>Chủ tịch nước</td>
+                        <td>Chính phủ</td>
+                        <td>250121232</td>
+                        <td>BIDV</td>
+                        <td>Hà Nội</td>
+                        <td></td>
+                    </tr>
+                    <!-- get data from var -->
+                    <tr v-for="employee in employees" :key='employee.employeeId' @dblclick="trOnDbClick()">
+                        <td></td>
+                        <td>{{employee.employeeCode}}</td>
+                        <td>{{employee.fullName}}</td>
+                        <td>{{employee.genderName}}</td>
+                        <td>{{ formatDateDDMMYYYYnew(employee.dateOfBirth) }}</td>
+                        <td>{{employee.identifyNumber}}</td>
+                        <td>{{employee.positionName}}</td>
+                        <td>{{employee.departmentName}}</td>
+                        <td>{{employee.bankAccount}}</td>
+                        <td>{{employee.bankName}}</td>
+                        <td>{{employee.bankBranch}}</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+    <!-- tranfer data to employeedetail -->
+        <EmployeeDetail
+        :isShow="isShowDialogDetail"
+        @hideDialog="hideDialog()"
+        />
+    </div>
+        
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import EmployeeDetail from "./EmployeeDetail.vue";
+
+export default {
+    components : {
+        EmployeeDetail,
+    },
+    created() {
+        // load du lieu
+        axios.get("http://localhost:8080/api/v1/Employee").then(res => {
+            this.employees = res.data;
+            console.log(res);
+        }).catch(res => {
+            console.log(res);
+        });
+    },
+    props : [],
+    methods : {
+        // double click vao 1 hang trong bang
+        trOnDbClick() {
+            this.isShowDialogDetail = true;
+        },
+        // click vao button add employee
+        btnAddOnClick() {
+            this.isShowDialogDetail = true;
+        },
+        // hien thi - an dialog
+        hideDialog() {
+            //an dialog
+            console.log(1);
+            this.isShowDialogDetail = false;
+        },
+        formatDateDDMMYYYYnew(date) {
+            //chuyen doi ngay thang de hien thi duoc
+            var newDate = new Date(date);
+            var dateString = newDate.getDate();
+            var monthString = newDate.getMonth() + 1;
+            var year = newDate.getFullYear();
+            var s = "";
+            var t = "";
+            if (dateString < 10) {
+                s = "0" + dateString.toString();
+            } else {
+                s = dateString.toString();
+            }
+            if (monthString < 10) {
+                t = "0" + monthString.toString();
+            } else {
+                t = monthString.toString();
+            }
+            return `${s}/${t}/${year}`;
+        }
+    },
+    data() {
+        return {
+            employees : [],
+            isShowDialogDetail: false,
+        };
+    },
+    watch : {},
+}
+</script>
+
+<style scoped>
+/* css input */
+.search-table {
+    height: 30px;
+    width: 200px;
+    border: 1px solid #bbbbbb;
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    border-radius: 4px;
+    outline: none;
+    box-sizing: border-box;
+    left: 200px;
+}
+
+.search-table:hover {
+        border-color: #019160;
+    }
+</style>
